b588ecfc181ce5d1bc00139edf2f2ac703a1c574
Ironblood9 <demirkan.cevik@gmail.com>
2025-03-15 19:08:56 +0300

    engine

---
+++ PrimalEngineEditor/GameProject/OpenProjectWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PrimalEngineEditor.GameProject
+{
+    /// <summary>
+    /// Interaction logic for OpenProjectWindow.xaml
+    /// </summary>
+    public partial class OpenProjectWindow : UserControl
+    {
+        public OpenProjectWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
