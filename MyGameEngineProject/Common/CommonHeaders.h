4bf096c743a7fadf3ea90c5e856f5cfe0ff71f29
Ironblood9 <demirkan.cevik@gmail.com>
2025-06-05 21:52:06 +0300

    Bits

---
+++ MyGameEngineProject/Common/CommonHeaders.h
@@ -0,0 +1,24 @@
+#pragma once
+
+// C/C++
+#include <stdint.h>
+#include <assert.h>
+#include<typeinfo>
+//Common headers
+#include "PrimitiveTypes.h"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ MyGameEngineProject/Components/ComponentsCommon.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "CommonHeaders.h" 
+#include "Id.h"
+
+namespace primal::game_entity {
+	DEFINE_TYPE_ID(entity_id);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
