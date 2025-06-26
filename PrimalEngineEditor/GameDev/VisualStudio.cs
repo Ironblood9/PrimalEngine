d0fec2801da3124108c667d20cd44c0e49b7fa1f
Ironblood9 <demirkan.cevik@gmail.com>
2025-06-26 11:44:21 +0300

    I FİNALLY SOLVED RELEASE ISSUE

--- PrimalEngineEditor/GameDev/VisualStudio.cs
+++ PrimalEngineEditor/GameDev/VisualStudio.cs
@@ -5,6 +5,7 @@ using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.ComTypes;
 using System;
 using System.Linq;
+using PrimalEngineEditor.GameProject;
 
 
 namespace PrimalEngineEditor.GameDev
@@ -13,6 +14,9 @@ namespace PrimalEngineEditor.GameDev
     {
         private static EnvDTE80.DTE2 _vsInstance = null;
         private static readonly string _programID = "VisualStudio.DTE";
+
+        public static bool BuildSucceeded { get; private set; } = true;
+        public static bool BuildDone { get; private set; } = true;
 
         [DllImport("ole32.dll")]
         private static extern int CreateBindCtx(uint reserved, out IBindCtx ppbc);
@@ -30,7 +34,7 @@ namespace PrimalEngineEditor.GameDev
                 if (_vsInstance == null)
                 {
                     //Find and Open VS
-                   var hResult= GetRunningObjectTable(0, out rot);
+                    var hResult = GetRunningObjectTable(0, out rot);
                     if (hResult < 0 || rot == null) throw new COMException($"GetRunningObjectTable() returned HRESULT: {hResult:X8}");
 
                     rot.EnumRunning(out monikerTable);
@@ -40,18 +44,18 @@ namespace PrimalEngineEditor.GameDev
                     if (hResult < 0 || bindCtx == null) throw new COMException($"CreateBindCtx() returned HRESULT: {hResult:X8}");
 
                     IMoniker[] currentMoniker = new IMoniker[1];
-                    while( monikerTable.Next(1, currentMoniker, IntPtr.Zero ) == 0)
+                    while (monikerTable.Next(1, currentMoniker, IntPtr.Zero) == 0)
                     {
                         string name = string.Empty;
                         currentMoniker[0]?.GetDisplayName(bindCtx, null, out name);
-                        if(name.Contains(_programID))
+                        if (name.Contains(_programID))
                         {
                             hResult = rot.GetObject(currentMoniker[0], out object obj);
                             if (hResult < 0 || obj == null) throw new COMException($"Running object table's GetObject() returned HRESULT: {hResult:X8}");
 
                             EnvDTE80.DTE2 dte = obj as EnvDTE80.DTE2;
                             var solutionName = dte.Solution.FullName;
-                            if(solutionName == solutionPath)
+                            if (solutionName == solutionPath)
                             {
                                 _vsInstance = dte;
                                 break;
@@ -66,7 +70,7 @@ namespace PrimalEngineEditor.GameDev
                     }
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Debug.WriteLine(ex.Message);
                 Logger.Log(MessageType.Error, "Failed to open Visual Studio");
@@ -80,7 +84,7 @@ namespace PrimalEngineEditor.GameDev
         }
         public static void CloseVS()
         {
-            if(_vsInstance?.Solution.IsOpen == true)
+            if (_vsInstance?.Solution.IsOpen == true)
             {
                 _vsInstance.ExecuteCommand("File.SaveAll");
                 _vsInstance.Solution.Close(true);
@@ -94,14 +98,14 @@ namespace PrimalEngineEditor.GameDev
             OpenVS(solution);
             try
             {
-                if(_vsInstance != null)
+                if (_vsInstance != null)
                 {
                     if (!_vsInstance.Solution.IsOpen) _vsInstance.Solution.Open(solution);
                     else _vsInstance.ExecuteCommand("File.SaveAll");
 
-                    foreach(EnvDTE.Project project in _vsInstance.Solution.Projects)
+                    foreach (EnvDTE.Project project in _vsInstance.Solution.Projects)
                     {
-                        if(project.UniqueName.Contains(projectName))
+                        if (project.UniqueName.Contains(projectName))
                         {
                             foreach (var file in files)
                             {
@@ -111,7 +115,7 @@ namespace PrimalEngineEditor.GameDev
                     }
 
                     var cpp = files.FirstOrDefault(x => Path.GetExtension(x) == ".cpp");
-                    if(!string.IsNullOrEmpty(cpp))
+                    if (!string.IsNullOrEmpty(cpp))
                     {
                         _vsInstance.ItemOperations.OpenFile(cpp, EnvDTE.Constants.vsViewKindTextView).Visible = true;
                     }
@@ -127,6 +131,69 @@ namespace PrimalEngineEditor.GameDev
                 return false;
             }
             return true;
+        }
+
+        private static void OnBuildSolutionDone(string project, string projectConfig, string platform, string solutionConfig, bool Success)
+        {
+            if (BuildDone) return;
+            if (Success) Logger.Log(MessageType.Info, $"Building {projectConfig} configuration succeeded");
+            else Logger.Log(MessageType.Error, $"Building {projectConfig} configuration failed");
+
+            BuildDone = true;
+            BuildSucceeded = Success;
+        }
+
+        private static void OnBuildSolutionBegin(string project, string projectConfig, string platform, string solutionConfig)
+        {
+            Logger.Log(MessageType.Info, $"Building {project}, {projectConfig}, {platform}, {solutionConfig}");
+        }
+
+        public static bool IsDebugging()
+        {
+            bool result = false;
+            for (int i = 0; i < 3; i++)
+            {
+                try
+                {
+                    result = _vsInstance != null && (_vsInstance.Debugger.CurrentProgram != null || _vsInstance.Debugger.CurrentMode ==
+                    EnvDTE.dbgDebugMode.dbgRunMode);
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                    if (!result) System.Threading.Thread.Sleep(1000);
+                }
+            }
+            return result;
+        }
+        public static void BuildSolution(NewProjectClass2 project, string configName, bool showWindow = true)
+        {
+            if (IsDebugging())
+            {
+                Logger.Log(MessageType.Error, "Visual Studio is running a process.");
+                return;
+            }
+            OpenVS(project.Solution);
+            BuildDone = BuildSucceeded = false;
+
+            for (int i = 0; i < 3; i++)
+            {
+                try
+                {
+                    if (!_vsInstance.Solution.IsOpen) _vsInstance.Solution.Open(project.Solution);
+                    _vsInstance.MainWindow.Visible = showWindow;
+                    _vsInstance.Events.BuildEvents.OnBuildProjConfigBegin += OnBuildSolutionBegin;
+                    _vsInstance.Events.BuildEvents.OnBuildProjConfigDone += OnBuildSolutionDone;
+                    _vsInstance.Solution.SolutionBuild.SolutionConfigurations.Item(configName).Activate();
+                    _vsInstance.ExecuteCommand("Build.BuildSolution");
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                    Debug.WriteLine($"Attempt {i}: failed to build {project.Name}");
+                    System.Threading.Thread.Sleep(1000);
+                }
+            }
         }
     }
 }
--- PrimalEngineEditor/GameProject/NewProjectClass2.cs
+++ PrimalEngineEditor/GameProject/NewProjectClass2.cs
@@ -16,65 +16,92 @@ using static System.Formats.Asn1.AsnWriter;
 
 namespace PrimalEngineEditor.GameProject
 {
-    [DataContract(Name ="Game")]
-     class NewProjectClass2: ViewModelBase
+    enum BuildConfiguraiton
     {
-        
+        Debug,
+        DebugEditor,
+        Release,
+        ReleaseEditor,
+    }
+    [DataContract(Name = "Game")]
+    class NewProjectClass2 : ViewModelBase
+    {
+
         public static string Extension { get; } = ".primal";
         [DataMember]
         public string Name { get; private set; } = "New Project";
         [DataMember]
 
-        public string  Path { get; private set; }
+        public string Path { get; private set; }
 
         public string FullPath => $@"{Path}{Name}{Extension}";
         public string Solution => $@"{Path}{Name}.sln";
-        [DataMember(Name ="Scenes")]
+
+        private static readonly string[] _buildConfigurationNames = new string[] { "Debug", "DebugEditor", "Release", "ReleaseEditor" };
+
+        private int _buildConfig;
+        [DataMember]
+        public int BuildConfig
+        {
+            get => _buildConfig;
+            set
+            {
+                if (_buildConfig != value)
+                {
+                    _buildConfig = value;
+                    OnPropertyChanged(nameof(BuildConfig));
+                }
+            }
+        }
+
+        public BuildConfiguraiton StandAloneBuildConfig => BuildConfig == 0 ? BuildConfiguraiton.Debug : BuildConfiguraiton.Release;
+        public BuildConfiguraiton DllBuildConfig => BuildConfig == 0 ? BuildConfiguraiton.DebugEditor : BuildConfiguraiton.ReleaseEditor;
+
+        [DataMember(Name = "Scenes")]
         private ObservableCollection<Scene> _scenes = new ObservableCollection<Scene>();
 
         public ReadOnlyObservableCollection<Scene> Scenes { get; private set; }
 
         public static NewProjectClass2 Current => Application.Current.MainWindow.DataContext as NewProjectClass2;
-        public  static UndoRedo UndoRedo { get; } = new UndoRedo();
+        public static UndoRedo UndoRedo { get; } = new UndoRedo();
 
         private Scene _activeScene;
-      
+
         public Scene ActiveScene
         {
             get => _activeScene;
             set
             {
-                if (_activeScene!=value)
+                if (_activeScene != value)
                 {
                     _activeScene = value;
                     OnPropertyChanged(nameof(ActiveScene));
                 }
             }
         }
-        public static  NewProjectClass2 Load(string file)
-        {
-            Debug.Assert(File.Exists(file));
-            return Serializer.FromFile<NewProjectClass2>(file);
-        }
+
         public ICommand UndoCommand { get; private set; }
         public ICommand RedoCommand { get; private set; }
         public ICommand AddNewSceneCommand { get; private set; }
         public ICommand RemoveSceneCommand { get; private set; }
         public ICommand SaveCommand { get; private set; }
+        public ICommand BuildCommand { get; private set; }
+
+        private static string GetConfigurationName(BuildConfiguraiton config) => _buildConfigurationNames[(int)config];
 
         private void AddNewSceneInternal(string sceneName)
         {
             Debug.Assert(!string.IsNullOrEmpty(sceneName.Trim()));
             _scenes.Add(new Scene(this, sceneName));
         }
-       
+
 
         private void RemoveSceneInternal(Scene scene)
         {
             Debug.Assert(_scenes.Contains(scene));
             _scenes.Remove(scene);
-           
-           
+
+
         }
         private int _sceneCounter = 1;
 
@@ -82,9 +109,6 @@ namespace PrimalEngineEditor.GameProject
         {
             return $"Scene {_sceneCounter++}";
         }
-       
-
-
 
         public void Unload()
         {
@@ -96,6 +120,41 @@ namespace PrimalEngineEditor.GameProject
             Serializer.ToFile(project, project.FullPath);
             Logger.Log(MessageType.Info, $"Project saved to {project.FullPath}");
         }
+
+        public static NewProjectClass2 Load(string file)
+        {
+            Debug.Assert(File.Exists(file));
+            return Serializer.FromFile<NewProjectClass2>(file);
+        }
+
+        private void BuildGameCodeDll(bool showWindow = true)
+        {
+            try
+            {
+                UnloadGameCodeDll();
+                VisualStudio.BuildSolution(this, GetConfigurationName(DllBuildConfig), showWindow);
+                if (VisualStudio.BuildSucceeded)
+                {
+                    LoadGameCodeDll();
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                throw;
+            }
+        }
+
+        private void LoadGameCodeDll()
+        {
+
+        }
+
+        private void UnloadGameCodeDll()
+        {
+
+        }
+
         [OnDeserialized]
         private void OnDeserialized(StreamingContext context)
         {
@@ -107,18 +166,18 @@ namespace PrimalEngineEditor.GameProject
             ActiveScene = Scenes.FirstOrDefault(x => x.IsActive);
 
             AddNewSceneCommand = new RelayCommand<object>(x =>
-             {
-             AddNewSceneInternal($" New Scene{_scenes.Count}");
-            var newScene = _scenes.Last();
-            var IndexScene = _scenes.Count - 1;
+            {
+                AddNewSceneInternal($" New Scene{_scenes.Count}");
+                var newScene = _scenes.Last();
+                var IndexScene = _scenes.Count - 1;
 
-            UndoRedo.Add(new UndoRedoActions(
-                () => RemoveSceneInternal(newScene),
-               () => _scenes.Insert(IndexScene, newScene),
-               $"Add{newScene.Name}"
-               ));
-             });
-         
+                UndoRedo.Add(new UndoRedoActions(
+                    () => RemoveSceneInternal(newScene),
+                   () => _scenes.Insert(IndexScene, newScene),
+                   $"Add{newScene.Name}"
+                   ));
+            });
+
 
 
             RemoveSceneCommand = new RelayCommand<Scene>(x =>
@@ -130,16 +189,16 @@ namespace PrimalEngineEditor.GameProject
                    () => RemoveSceneInternal(x),
                    $"Remove {x.Name}"));
 
-            }, x=> !x.IsActive
+            }, x => !x.IsActive
             );
 
-            UndoCommand = new RelayCommand<object>(x => UndoRedo.Undo());
-            RedoCommand = new RelayCommand<object>(x => UndoRedo.Redo());
+            UndoCommand = new RelayCommand<object>(x => UndoRedo.Undo(), x => UndoRedo.UndoList.Any());
+            RedoCommand = new RelayCommand<object>(x => UndoRedo.Redo(), x => UndoRedo.RedoList.Any());
             SaveCommand = new RelayCommand<object>(x => Save(this));
-
+            BuildCommand = new RelayCommand<bool>(x => BuildGameCodeDll(x), x => !VisualStudio.IsDebugging() && VisualStudio.BuildDone);
         }
         public NewProjectClass2(string name, string path)
-        { 
+        {
             Name = name;
             Path = path;
             OnDeserialized(new StreamingContext());
