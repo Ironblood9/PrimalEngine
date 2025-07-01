d5a7ddfebbeff0b245658db606348aacb5ed885a
Demirkan Çevik <demirkancevik48@gmail.com>
2025-07-01 11:08:26 +0300

    Loading Function from game code DLL

--- AgilisDLL/AgilisAPI.cpp
+++ AgilisDLL/AgilisAPI.cpp
@@ -1,6 +1,6 @@
 #include"CommonDll.h"
 #include"CommonHeaders.h"
-
+#include "..\MyGameEngineProject\Components\Script.h"
 #ifndef WIN32_MEAN_AND_LEAN
 #define WIN32_MEAN_AND_LEAN
 #endif
@@ -11,7 +11,10 @@ using namespace primal;
 
 namespace {
 	HMODULE game_code_dll{ nullptr };
-
+	using _get_script_creator = primal::script::detail::script_creator(*)(size_t);
+	_get_script_creator get_script_creator{ nullptr };
+	using _get_script_names = LPSAFEARRAY(*)(void);
+	_get_script_names get_script_names{ nullptr };
 }//anonymous
 
 EDITOR_INTERFACE u32
@@ -20,7 +23,11 @@ LoadGameCodeDll(const char* dll_path)
 	if (game_code_dll) return FALSE;
     game_code_dll=LoadLibraryA(dll_path);
 	assert(game_code_dll);
-	return game_code_dll ? TRUE : FALSE;
+
+	get_script_creator =(_get_script_creator)GetProcAddress(game_code_dll, "get_script_creator");
+	get_script_names = (_get_script_names)GetProcAddress(game_code_dll, "get_script_names");
+
+	return (game_code_dll && get_script_creator && get_script_names) ? TRUE : FALSE;
 }
 
 EDITOR_INTERFACE u32
@@ -32,4 +39,16 @@ UnloadGameCodeDll()
 	assert(result);
 	game_code_dll = nullptr;
 	return TRUE;
+}
+
+EDITOR_INTERFACE script::detail::script_creator
+GetScriptCreator(const char* name)
+{
+	return(game_code_dll && get_script_creator) ? get_script_creator(script::detail::string_hash()(name)) : nullptr;
+}
+
+EDITOR_INTERFACE LPSAFEARRAY
+GetScriptNames()
+{
+	return (game_code_dll && get_script_names) ? get_script_names() : nullptr;
 }
--- PrimalEngineEditor/DllWrappers/AgilisAPI.cs
+++ PrimalEngineEditor/DllWrappers/AgilisAPI.cs
@@ -18,9 +18,16 @@ namespace PrimalEngineEditor.AgilisAPIStructs
         public Vector3 Scale = new Vector3(1, 1, 1);
     }
     [StructLayout(LayoutKind.Sequential)]
+    class ScriptComponent
+    {
+        public IntPtr ScriptCreator;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
     class GameEntityDescriptor
     {
         public TransformComponent Transform = new TransformComponent();
+        public ScriptComponent Script = new ScriptComponent();
     }
 
 }
@@ -37,6 +44,12 @@ namespace PrimalEngineEditor.DllWrappers
         [DllImport(_engineDll)]
         public static extern int UnloadGameCodeDll();
 
+        [DllImport(_engineDll)]
+        public static extern IntPtr GetScriptCreator(string name);
+        [DllImport(_engineDll)]
+        [return: MarshalAs(UnmanagedType.SafeArray)]
+        public static extern string[] GetScriptNames();
+
         internal static class EntityAPI
         {
             [DllImport(_engineDll)]
@@ -50,6 +63,10 @@ namespace PrimalEngineEditor.DllWrappers
                     desc.Transform.Position = c.Position;
                     desc.Transform.Rotation = c.Rotation;
                     desc.Transform.Scale = c.Scale;
+                }
+                //script component
+                {
+                    var c = entity.GetComponent<Script>();
                 }
                 return CreateGameEntity(desc);
             }
