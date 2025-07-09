51d4deaa61aac946c69ea298d39488e5ac13e9fd
Demirkan Çevik <demirkancevik48@gmail.com>
2025-07-09 12:39:56 +0300

    Platfrom Windows

--- AgilisPrimeEngineTest/Main.cpp
+++ AgilisPrimeEngineTest/Main.cpp
@@ -1,20 +1,52 @@
 #pragma comment(lib, "mygameengineproject.lib")
 
 
-#define TEST_ENTITY_COMPONENTS 1
+#define TEST_ENTITY_COMPONENTS 0
+#define TEST_WINDOW 1
 
 #if TEST_ENTITY_COMPONENTS
 #include "TestEntityComponents.h"
+#elif TEST_WINDOW
+#include "TestWindow.h"
 #else
 #error One of the tests need to be enabled
 #endif // !TEST_ENTITY_COMPONENTS
 
+#ifdef _WIN64
+#include<Windows.h>
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+#if _DEBUG
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+	agilis_test test{};
+
+	if (test.initialize())
+	{
+		MSG msg{};
+		bool is_running{ true };
+		while (is_running)
+		{
+			while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+			{
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+				is_running &= (msg.message != WM_QUIT);
+			}
+			test.run();
+		}
+	}
+	test.shutdown();
+	return 0;
+}
+
+#else
 int main()
 {
 	#if _DEBUG
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
-	engine_test test{};
+	agilis_test test{};
 	if (test.initialize()) 
 	{
 		test.run();
@@ -22,3 +54,4 @@ int main()
 	}
 	test.shutdown();
 }
+#endif // _WIN64
--- AgilisPrimeEngineTest/TestEntityComponents.h
+++ AgilisPrimeEngineTest/TestEntityComponents.h
@@ -10,7 +10,7 @@
 
 using namespace primal;
 
-class engine_test : public test
+class agilis_test : public test
 {
 public:
 	bool initialize() override 
