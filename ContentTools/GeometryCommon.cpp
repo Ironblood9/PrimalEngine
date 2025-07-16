f4f43a085cf1350227e5ff405026e8f458b17bed
Demirkan Çevik <demirkancevik48@gmail.com>
2025-07-16 12:17:47 +0300

    pack_vertices

--- ContentTools/GeometryCommon.cpp
+++ ContentTools/GeometryCommon.cpp
@@ -81,8 +81,65 @@ namespace primal::tools {
 					XMStoreFloat3(&vertex.normal, XMVector3Normalize(n1));
 				}
 			}
+		}
+
+		void process_uvs(mesh& mesh)
+		{
+			utl::vector<vertex> old_vertices;
+			old_vertices.swap(mesh.vertices);
+			utl::vector<u32> old_indices(mesh.indices.size());
+			old_indices.swap(mesh.indices);
+
+			const u32 num_vertices{ (u32)old_vertices.size() };
+			const u32 num_indices{ (u32)old_indices.size() };
+			assert(num_vertices && num_indices);
+
+			utl::vector<utl::vector<u32>> idx_ref(num_vertices);
+			for (u32 i{ 0 }; i < num_indices; i++)
+				idx_ref[old_indices[i]].emplace_back(i);
+
+			for (u32 i{ 0 }; i < num_indices; i++)
+			{
+				auto& refs{ idx_ref[i] };
+				u32 num_refs{ (u32)refs.size() };
+				for (u32 j{ 0 }; j < num_refs; j++)
+				{
+					mesh.indices[refs[j]] = (u32)mesh.vertices.size();
+					vertex& vertex{ old_vertices[old_indices[refs[j]]] };
+					vertex.uv = mesh.uv_sets[0][refs[j]];
+					mesh.vertices.emplace_back(vertex);
+
+					for (u32 k{ j + 1 }; k < num_refs; k++)
+					{
+						v2& uv1{ mesh.uv_sets[0][refs[k]] };
+						if (XMScalarNearEqual(vertex.uv.x, uv1.x, epsilon) && XMScalarNearEqual(vertex.uv.y, uv1.y, epsilon))
+						{
+							mesh.indices[refs[k]] = mesh.indices[refs[j]];
+							refs.erase(refs.begin() + k);
+							--num_refs;
+							--k;
+						}
+					}
+				}
+			}
 
 		}
+
+		void pack_vertices_static(mesh& mesh)
+		{
+			const u32 num_vertices{ (u32)mesh.vertices.size() };
+			assert(num_vertices);
+			mesh.packed_vertices_static.reserve(num_vertices);
+
+			for (u32 i{ 0 }; i < num_vertices; i++)
+			{
+				vertex& vertex{ mesh.vertices[i] };
+				const u8 signs{ (u8)((vertex.normal.z > 0.f) << 1) };
+				const u8 normal_x{ (u16)pack_float<16>(vertex.normal.x, -1.f, 1.f) };
+				const u8 normal_y{ (u16)pack_float<16>(vertex.normal.y, -1.f, 1.f) };
+			}
+		}
+
 
 		void process_vertices(mesh& mesh, const geometry_import_settings& settings)
 		{
--- ContentTools/GeometryCommon.h
+++ ContentTools/GeometryCommon.h
@@ -3,6 +3,19 @@
 
 namespace primal::tools
 {
+	namespace packed_vertex
+	{
+		struct vertex_static
+		{
+			math::v3       position;
+			u8             reserved[3];
+			u8             t_sign; // bit 0; tangent handedness * (tangent.z sign), bit 1: normal.z sign ( 0 means -1, 1 means +1)
+			u16            normal[2];
+			u16            tangent[2];
+			math::v2       uv;
+		};
+	}//packed_vertex
+
 	struct vertex
 	{
 		math::v4     tangent{};
@@ -22,7 +35,12 @@ namespace primal::tools
 		// intermediate(orta) data
 		utl::vector<vertex>                          vertices;
 		utl::vector<u32>                             indices;
+
 		// output data
+		std::string                                  name;
+		utl::vector<packed_vertex::vertex_static>    packed_vertices_static;
+		f32                                          lod_threshold{ -1.f };
+		u32                                          lod_id{ u32_invalid_id };
 	};
 
 	struct lod_group
