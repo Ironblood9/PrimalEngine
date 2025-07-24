f6d5cecb58b65f37038bc7ba4f5a235b8ddfc1f9
Demirkan Çevik <demirkancevik48@gmail.com>
2025-07-24 12:30:59 +0300

    Textures

--- PrimalEngineEditor/Content/PrimitiveMeshDialog.xaml.cs
+++ PrimalEngineEditor/Content/PrimitiveMeshDialog.xaml.cs
@@ -4,6 +4,7 @@ using PrimalEngineEditor.DllWrappers;
 using PrimalEngineEditor.Utilities.Controls;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,7 +24,7 @@ namespace PrimalEngineEditor.Content
     /// </summary>
     public partial class PrimitiveMeshDialog : Window
     {
-
+        private static readonly List<ImageBrush> _textures = new List<ImageBrush>();
         private void OnPrimitiveType_ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) => UpdatePrimitive();
 
         private void OnSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) => UpdatePrimitive();
@@ -53,26 +54,73 @@ namespace PrimalEngineEditor.Content
                         break;
                     }
                 case PrimitiveMeshType.Cube:
-                    break;
+                    return;
                 case PrimitiveMeshType.UvSphere:
-                    break;
+                    return;
                 case PrimitiveMeshType.IcoSphere:
-                    break;
+                    return; 
                 case PrimitiveMeshType.Cylinder:
-                    break;
+                    return;
                 case PrimitiveMeshType.Capsule:
-                    break;
+                    return;
                 default:
                     break;
             }
             var geometry = new Geometry();
             ContentToolsAPI.CreatePrimitiveMesh(geometry, info);
             (DataContext as GeometryEditor).SetAsset(geometry);
+            OnTexture_CheckBox_Click(textureCheckBox, null);
         }
+
+        private static void LoadTextures()
+        {
+            var uris = new List<Uri>
+            {
+                new Uri("pack://application:,,,/Resources/PrimitiveMeshWindow/PlaneTexture.png"),
+            };
+
+            _textures.Clear();
+            foreach (var uri in uris)
+            {
+                var resource = Application.GetResourceStream(uri);
+                using var reader = new BinaryReader(resource.Stream);
+                var data = reader.ReadBytes((int)resource.Stream.Length);
+                var imageSource = (BitmapSource)new ImageSourceConverter().ConvertFrom(data);
+                imageSource.Freeze();
+
+                var brush = new ImageBrush(imageSource);
+                brush.Transform = new ScaleTransform(1, -1, 0.5, 0.5);
+                brush.ViewportUnits = BrushMappingMode.Absolute;
+                brush.Freeze();
+
+                _textures.Add(brush);
+            }
+
+        }
+
+        static PrimitiveMeshDialog()
+        {
+            LoadTextures();
+        }
+
         public PrimitiveMeshDialog()
         {
             InitializeComponent();
             Loaded += (s, e) => UpdatePrimitive();
+        }
+
+        private void OnTexture_CheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            Brush brush = Brushes.White;
+            if ((sender as CheckBox).IsChecked == true)
+            {
+                brush = _textures[(int)primTypeComboBox.SelectedItem];
+            }
+            var vm = DataContext as GeometryEditor;
+            foreach (var mesh in vm.MeshRenderer.Meshes)
+            {
+                mesh.Diffuse = brush;
+            }
         }
     }
 }
