27cdc71b9649b06f588e86317df110725dfc7ca6
Demirkan Çevik <demirkancevik48@gmail.com>
2025-07-28 13:20:12 +0300

    D3D12

---
+++ MyGameEngineProject/Graphics/Direct3D12/D3D12Core.cpp
@@ -0,0 +1,16 @@
+#include "D3D12Core.h"
+
+
+namespace primal::graphics::d3d12::core
+{
+	namespace {
+		ID3D12Device8* main_device;
+	}//anonymous
+
+	bool initialize()
+	{
+		// determine which adapter to use
+		// determine what is the maximum feature level that is supporter
+		// create a ID3D12Device
+	}
+}
--- MyGameEngineProject/Graphics/Renderer.cpp
+++ MyGameEngineProject/Graphics/Renderer.cpp
@@ -9,9 +9,25 @@ namespace primal::graphics
 
 		bool set_platform_interface(graphics_platform platform)
 		{
-			
+			switch (platform)
+			{
+			case graphics_platform::direct3d12:
+				d3d12::get_platform_interface(gfx);
+				break;
+			default:
+				break;
+			}
 		}
 
 	}//anonymous
+
+	bool initialize(graphics_platform platform)
+	{
+		return set_platform_interface(platform);
+	}
+	void shutdown()
+	{
+		gfx.shutdown();
+	}
 }
 
