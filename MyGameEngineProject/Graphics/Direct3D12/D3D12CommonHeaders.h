f77d4c4711cbfc22bb9d834d6e98c26bf685a9d7
Demirkan Çevik <demirkancevik48@gmail.com>
2025-07-29 11:50:54 +0300

    Minimum feature level

--- MyGameEngineProject/Graphics/Direct3D12/D3D12CommonHeaders.h
+++ MyGameEngineProject/Graphics/Direct3D12/D3D12CommonHeaders.h
@@ -8,3 +8,31 @@
 
 #pragma comment(lib, "dxgi.lib")
 #pragma comment(lib, "d3d12.lib")
+
+
+// Asset that COM call to D3D API succeeded
+
+#ifdef _DEBUG
+
+#ifndef DXCall
+#define DXCall(x)                                         \
+if(FAILED(x)) {                                           \
+   char line_number[32];                                  \
+   sprintf_s(line_number, "%u", __LINE__);                \
+   OutputDebugStringA("Error in: ");                      \
+   OutputDebugStringA(__FILE__);                          \
+   OutputDebugStringA("\nLine: ");                        \
+   OutputDebugStringA(line_number);                       \
+   OutputDebugStringA("\n");                              \
+   OutputDebugStringA(#x);                                \
+   OutputDebugStringA("\n");                              \
+   __debugbreak();                                        \
+}
+#endif // !DXCall
+#else
+#ifndef DXCall
+#define DXCall(x) x
+#endif // !DXCall
+#endif // _DEBUG
+
+
--- MyGameEngineProject/Graphics/Direct3D12/D3D12Core.cpp
+++ MyGameEngineProject/Graphics/Direct3D12/D3D12Core.cpp
@@ -1,16 +1,60 @@
 #include "D3D12Core.h"
 
+using namespace Microsoft::WRL;
 
 namespace primal::graphics::d3d12::core
 {
 	namespace {
-		ID3D12Device8* main_device;
+		ID3D12Device8* main_device{ nullptr };
+		IDXGIFactory7* dxgi_factory{ nullptr };
+
+		constexpr D3D_FEATURE_LEVEL minimum_feature_level{ D3D_FEATURE_LEVEL_11_0 };
+
+		bool failed_init()
+		{
+			shutdown();
+			return false;
+		}
+
+		IDXGIAdapter4*
+		determine_main_adapter()
+		{
+			IDXGIAdapter4* adapter{ nullptr };
+			// get adapters in descending order of performance
+			for (u32 i{ 0 }; dxgi_factory->EnumAdapterByGpuPreference(i, DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE, IID_PPV_ARGS(&adapter)) != DXGI_ERROR_NOT_FOUND;
+			++i)
+			{
+				// pick the first adapter that supports the minimum feature level
+				if (SUCCEEDED(D3D12CreateDevice(adapter, minimum_feature_level, __uuidof(ID3D12Device), nullptr)))
+				{
+					return adapter;
+				}
+				release(adapter);
+			}
+			return nullptr;
+		}
+
 	}//anonymous
 
 	bool initialize()
 	{
-		// determine which adapter to use
 		// determine what is the maximum feature level that is supporter
 		// create a ID3D12Device
+		if (main_device) shutdown();
+
+		u32 dxgi_factory_flags{ 0 };
+#ifdef _DEBUG
+		dxgi_factory_flags |= DXGI_CREATE_FACTORY_DEBUG;
+#endif // _DEBUG
+
+		HRESULT hr{ S_OK };
+		hr = CreateDXGIFactory2(dxgi_factory_flags, IID_PPV_ARGS(&dxgi_factory)); // uuid universally unique identifier
+		if (FAILED(hr)) return failed_init();
+
+		// determine which adapter to use, if any
+		ComPtr<IDXGIAdapter4> main_adapter;
+		main_adapter.Attach(determine_main_adapter());
+
+		if (!main_adapter) return failed_init();
 	}
 }
