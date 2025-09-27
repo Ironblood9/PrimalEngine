6d9e6218f4687de284c14e9a73f4973d908b4bb7
Demirkan Çevik <demirkancevik48@gmail.com>
2025-09-27 11:55:40 +0300

    Finishing the Camera

--- MyGameEngineProject/Components/Transform.cpp
+++ MyGameEngineProject/Components/Transform.cpp
@@ -4,11 +4,20 @@
 namespace primal::transform 
 {
 	namespace {
-
-		utl::vector< math::v3 > positions;
 		utl::vector<  math::v4 > rotations;
+		utl::vector< math::v3 > orientations;
+		utl::vector< math::v3 > positions;
 		utl::vector< math::v3 > scales;
 
+		math::v3 calculate_orientation(math::v4 rotation)
+		{
+			using namespace DirectX;
+			XMVECTOR rotation_quat{ XMLoadFloat4(&rotation) };
+			XMVECTOR front{ XMVectorSet(0.f, 0.f, 1.f, 0.f) };
+			math::v3 orientation;
+			XMStoreFloat3(&orientation, XMVector3Rotate(front, rotation_quat));
+			return orientation;
+		}
 	}//anoymous namespace
 	component 
 	create(init_info info, game_entity::entity entity)
@@ -18,14 +27,17 @@ namespace primal::transform
 
 		if (positions.size() > entity_index) 
 		{
-			rotations[entity_index] = math::v4(info.rotation);
-			positions[entity_index] = math::v3(info.position);
-			scales[entity_index] = math::v3(info.scale);
+			math::v4 rotation{ info.rotation };
+			rotations[entity_index] = rotation;
+			orientations[entity_index] = calculate_orientation(rotation);
+			positions[entity_index] = math::v3{ info.position };
+			scales[entity_index] = math::v3{ info.scale };
 		}
 		else
 		{
 			assert(positions.size() == entity_index);
 			rotations.emplace_back(info.rotation);
+			orientations.emplace_back(calculate_orientation(math::v4{ info.rotation }));
 			positions.emplace_back(info.position);
 			scales.emplace_back(info.scale);
 		}
@@ -41,6 +53,14 @@ namespace primal::transform
 		assert(is_valid());
 		return rotations[id::index(_id)];
 	}
+
+	math::v3 
+	component::orientation() const
+	{
+		assert(is_valid());
+		return orientations[id::index(_id)];
+	}
+
 	math::v3
 	component::position() const
 	{
